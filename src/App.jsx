bea4bdae17757604aaf74d0658654e73d368fd64
Dageshwar Das <109011384+Dageshwar07@users.noreply.github.com>
2024-11-02 23:17:53 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,22 +2,6 @@ import { useState } from 'react';
 
 function App() {
 
-  // const [firstName, setFirstName] = useState("");
-  // const [lastName, setLastName] = useState("");
-
-  // console.log(firstName);
-  // console.log(lastName);
-  // function changeFirstNameHandler(event) {
-  //   // console.log("printing first name")
-  //   // console.log(event.target.value);
-  //   setFirstName(event.target.value);
-  // }
-  // function changelastNameHandler(event) {
-  //   // console.log("Printing last name")
-  //   // console.log(event.target.value);
-  //   setLastName(event.target.value);
-  // }
-
   const [formData, setFormData] = useState({
     firstName: "",
     lastName: "", email: "", comments: "", isVisible: true, mode: "", favCar: ""
